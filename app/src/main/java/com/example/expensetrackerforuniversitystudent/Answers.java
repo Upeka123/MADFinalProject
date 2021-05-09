dbee06905c894cafd2eeda8fe41fba120d8e54b7
pasindubhasura <srmpasindubhasura@gmail.com>
2021-05-09 17:19:00 +0530

    Add shared preference to save,retrieve and delete student id

--- app/src/main/java/com/example/expensetrackerforuniversitystudent/Answers.java
+++ app/src/main/java/com/example/expensetrackerforuniversitystudent/Answers.java
@@ -1,7 +1,9 @@
 package com.example.expensetrackerforuniversitystudent;
 
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -35,6 +37,10 @@ public class Answers extends AppCompatActivity {
     Button edtBtn, dltBtn;
     AnswerAdapter answerAdapter;
     RecyclerView recyclerView;
+    String stuID;
+    private final static String MyPref = "MyPref";
+    SharedPreferences sharedPreferences;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,6 +48,11 @@ public class Answers extends AppCompatActivity {
         toolbar = findViewById(R.id.toolbar);
         toolbar.setTitle("Answers");
         setSupportActionBar(toolbar);
+
+        sharedPreferences  = getSharedPreferences(MyPref, Context.MODE_PRIVATE);
+        SharedPreferences.Editor myEdit = sharedPreferences.edit();
+
+        stuID = sharedPreferences.getString("stuID", null);
 
         idNo = findViewById(R.id.answers_question_item_studentID);
         question = findViewById(R.id.answers_question_item_question);
